cb6aaf2dc5a9178dcf757f78848dd796bd405373
Treb Ryan <trebryan@gmail.com>
2015-01-11 09:11:46 -0800

    Working on chaning the pixels.

--- scoreform.php
+++ scoreform.php
@@ -10,7 +10,7 @@
 	  //TODO this should create default score.json on first run (w+?) does it?
 	  //TODO why pass "red", "blue"? Is the intent to have other colors?
 		$scoreFile = 'data/score.json';
-		$fh = fopen($scoreFile, 'w') or die("impossible to open file");
+		$fh = fopen($scoreFile, 'w+') or die("impossible to open file");
 	    $scoreData = array($_POST['color0'] => intval($_POST['score0']), $_POST['color1'] => intval($_POST['score1']));
 		//$scoreData = $_POST['redScore'];
 		$scoreData=json_encode($scoreData);
